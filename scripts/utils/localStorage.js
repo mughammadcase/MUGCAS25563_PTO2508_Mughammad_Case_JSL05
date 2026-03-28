75efd4f62adaf796a34392da10d1036a8c350024
Mughammad <caseameen44@gmail.com>
2026-03-29 01:49:02 +0200

    Adds localStorage handling for tasks
    - Loads tasks from storage with fallback to initial data
    
    - Saves tasks to localStorage

--- scripts/utils/localStorage.js
+++ scripts/utils/localStorage.js
@@ -1,1 +1,27 @@
- 
+import { initialTasks } from "../../initialData.js";
+
+/**
+ * Loads tasks from localStorage.
+ * If no tasks are found, fall back to initialTasks and save to localStorage.
+ * @returns {Array} Array of tasks from localStorage/initialTasks
+ */
+export function loadTasksFromLocalStorage() {
+  const stored = localStorage.getItem("tasks");
+
+  if (stored)
+    try {
+      return JSON.parse(stored);
+    } catch (error) {
+      console.error("Error parsing tasks from localStorage:", error);
+    }
+  localStorage.setItem("tasks", JSON.stringify(initialTasks));
+  return initialTasks;
+}
+
+/**
+ * Saves tasks to localStorage.
+ * @param {Array} tasks - Array of tasks to save to localStorage
+ */
+export function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
